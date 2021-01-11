605e5a457351c78d204bc45a958076ba776309e2
Astig-1982 <astig.mandalian2014@gmail.com>
2021-01-11 20:18:41 +0000

    Added query functionality

--- services/views.py
+++ services/views.py
@@ -1,4 +1,6 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect, reverse
+from django.contrib import messages
+from django.db.models import Q
 from .models import Services
 
 # Create your views here.
@@ -7,6 +9,17 @@ from .models import Services
 def all_services(request):
 
     services = Services.objects.all()
+    query = None
+
+    if request.GET:
+        if 'q' in request.GET:
+            query = request.GET['q']
+            if not query:
+                messages.error(request, "No entry has been dedected")
+                return redirect(reverse('services'))
+
+            queries = Q(name__icontains=query) | Q(description__icontains=query) | Q(short_description__icontains=query)
+            services = services.filter(queries)
 
     context = {
         'services': services,
