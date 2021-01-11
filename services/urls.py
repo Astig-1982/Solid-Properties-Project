6a3e687168bcb9979f5540de1940388808ecd887
Astig-1982 <astig.mandalian2014@gmail.com>
2021-01-11 19:39:02 +0000

    Created view and url for detailed_service

--- services/urls.py
+++ services/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.all_services, name='services')
+    path('', views.all_services, name='services'),
+    path('detailed_service/<service_id>', views.detailed_service, name='detailed_service')
 ]
--- services/views.py
+++ services/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Services
 
 # Create your views here.
@@ -13,5 +13,16 @@ def all_services(request):
     }
 
     return render(request, 'services/services.html', context)
+
+
+def detailed_service(request, service_id):
+
+    service = get_object_or_404(Services, pk=service_id)
+
+    context = {
+        'service': service,
+    }
+
+    return render(request, 'services/detailed_service.html', context)
 
 
