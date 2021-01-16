76b1aaf792cfc6537f3baefe081507ead7715998
Astig-1982 <astig.mandalian2014@gmail.com>
2021-01-16 12:19:38 +0000

    Added category images at home page

--- home/views.py
+++ home/views.py
@@ -1,7 +1,15 @@
 from django.shortcuts import render
+from services.models import Category
 
 # Create your views here.
 
 
 def index(request):
-    return render(request, "home/index.html")
+
+    categories = Category.objects.all()
+
+    context = {
+        'categories': categories
+    }
+
+    return render(request, "home/index.html", context)
