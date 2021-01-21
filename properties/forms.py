79000285f84b97556d443d1e90097bada14974d5
Astig-1982 <astig.mandalian2014@gmail.com>
2021-01-21 20:24:14 +0000

    Created property form

---
+++ properties/forms.py
@@ -0,0 +1,11 @@
+from django import forms
+from .models import Properties
+
+
+class PropertyForm(forms.ModelForm):
+    class Meta:
+        model = Properties
+        fields = ('street_address', 'house_name',
+                  'post_code', 'no_of_bedrooms')
+
+
--- properties/views.py
+++ properties/views.py
@@ -1,3 +1,35 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect, reverse
+from .forms import PropertyForm
+from profiles.models import LandlordProfile
 
 # Create your views here.
+
+
+def add_property(request):
+
+    landlord = get_object_or_404(LandlordProfile, user=request.user)
+
+    if request.method == 'POST':
+        form_data = {
+                'landlord': request.user,
+                'street_address': request.POST['street_address'],
+                'house_name': request.POST['house_name'],
+                'post_code': request.POST['post_code'],
+                'no_of_bedrooms': request.POST['no_of_bedrooms'],
+            }
+
+        property_form = PropertyForm(form_data)
+
+        if property_form.is_valid():
+            property_form.save()
+
+    else:
+        property_form = PropertyForm()
+
+    context = {
+        'landlord': landlord,
+        'property_form': property_form,
+    }
+
+    return render(request, 'add_property/add_property.html', context)
+
