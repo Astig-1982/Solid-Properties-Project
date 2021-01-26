d89967bc51e1ef0b4f4681416224be841a8afe90
Astig-1982 <astig.mandalian2014@gmail.com>
2021-01-26 21:29:01 +0000

    Created 'add_to_cart' view and created form for adding the service to the cart

--- cart/urls.py
+++ cart/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.view_cart, name='cart'),
+    path('add_to_cart/<service_id>', views.add_to_cart, name='add_to_cart'),
 ]
--- cart/views.py
+++ cart/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
+from services.models import Services
 
 # Create your views here.
 
@@ -8,4 +9,26 @@ def view_cart(request):
     context = {}
 
     return render(request, 'cart/cart.html', context)
+
+
+def add_to_cart(request, service_id):
+
+    service = get_object_or_404(Services, pk=service_id)
+    redirect_url = request.POST.get('redirect_url')
+
+    if service.price_variation:
+        no_of_bedrooms = int(request.POST.get('no_of_bedrooms'))
+    else:
+        no_of_bedrooms = 1
+
+    cart = request.session.get('cart', {})
+
+    cart[service_id] = no_of_bedrooms
+    request.session['cart'] = cart
+    print(request.session['cart'])
+
+    return redirect(redirect_url)
+
+
+
 
