d8c31511fe65e65839e3a090e2005f1c56fd1d55
Astig-1982 <astig.mandalian2014@gmail.com>
2021-02-06 11:58:47 +0000

    Added tables for displaying the profile page and styled the page

--- profiles/views.py
+++ profiles/views.py
@@ -10,10 +10,12 @@ def landlord_profile(request):
     landlord = get_object_or_404(LandlordProfile, user=request.user)
     all_properties = landlord.properties.all()
     properties = all_properties.order_by('street_address')
+    properties_count = all_properties.count()
 
     context = {
         'landlord': landlord,
         'properties': properties,
+        'properties_count': properties_count,
     }
 
     return render(request, 'profiles/profile.html', context)
