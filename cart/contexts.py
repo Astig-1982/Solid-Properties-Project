edcbca773928ed66a7535d78d955e8c76eca0a2a
Astig-1982 <astig.mandalian2014@gmail.com>
2021-02-10 21:01:17 +0000

    Retrieved the property

--- cart/contexts.py
+++ cart/contexts.py
@@ -1,6 +1,6 @@
-from django.conf import settings
-from django.shortcuts import get_object_or_404, render
+from django.shortcuts import get_object_or_404
 from services.models import Services
+from properties.models import Properties
 
 
 def cart_contents(request):
@@ -17,11 +17,11 @@ def cart_contents(request):
         for address_or_no_of_bedrooms in addresses_or_bedrooms:
             if request.user.is_authenticated:
                 for key_address, no_of_bedrooms in address_or_no_of_bedrooms.items():
+                    the_property = get_object_or_404(Properties, street_address=key_address)
                     total_cost = no_of_bedrooms * float(service.price)
                     total.append(total_cost)
                     list_addresses_or_bedrooms.append({
-                        "address": key_address,
-                        "no_of_bedrooms": no_of_bedrooms,
+                        "the_property": the_property,
                         "total_cost": format(total_cost, '.2f'),
                     })
             else:
@@ -46,70 +46,4 @@ def cart_contents(request):
             "grand_total": grand_total,
         }
 
-    print(cart_items)
-
     return context
-
-
-#def cart_contents(request):
-#
-#    cart_items = []
-#    cart = request.session.get('cart', {})
-#    grand_total = 0
-#
-#    if request.user.is_authenticated:
-#        grand_total = []
-#        for service, addresses in cart.items():
-#            total = []
-#            list_addresses = []
-#            service = get_object_or_404(Services, pk=service)
-#            for address in addresses:
-#                for key_address, no_of_bedrooms in address.items():
-#                    total_cost = no_of_bedrooms * float(service.price)
-#                    total.append(total_cost)
-#                    list_addresses.append({
-#                        "address": key_address,
-#                        "no_of_bedrooms": no_of_bedrooms,
-#                        "total_cost": format(total_cost, '.2f'),
-#                    })
-#            grand_total.append(sum(total))
-#            total = format(sum(total), '.2f')
-#            cart_items.append({
-#                "service": service,
-#                "properties": list_addresses,
-#                "total_for_all_properties": total,
-#            })
-#
-#    else:
-#        grand_total = []
-#        for service, bedrooms in cart.items():
-#            total = []
-#            list_bedrooms = []
-#            service = get_object_or_404(Services, pk=service)
-#            for no_of_bedrooms in bedrooms:
-#                total_cost = no_of_bedrooms * float(service.price)
-#                total.append(total_cost)
-#                list_bedrooms.append({
-#                    "no_of_bedrooms": no_of_bedrooms,
-#                    "total_cost": total_cost,
-#                })
-#            grand_total.append(sum(total))
-#            total = format(sum(total), '.2f')
-#            cart_items.append({
-#                "service": service,
-#                "list_bedrooms": list_bedrooms,
-#                "total_for_all_properties": total,
-#            })
-#
-#
-#   grand_total = format(sum(grand_total), '.2f')
-#
-#    context = {
-#            "cart_items": cart_items,
-#            "grand_total": grand_total,
-#        }
-#
-#    print(cart_items)
-#
-#    return context
-#
