8ffdc6d037d52b07775092a4bdb6a62aaba297d0
Astig-1982 <astig.mandalian2014@gmail.com>
2021-02-11 20:35:51 +0000

    Changed the shopping cart logic

--- cart/contexts.py
+++ cart/contexts.py
@@ -10,37 +10,33 @@ def cart_contents(request):
     grand_total = 0
 
     grand_total = []
-    for service, addresses_or_bedrooms in cart.items():
+
+    for service, properties_or_bedrooms in cart.items():
         total = []
-        list_addresses_or_bedrooms = []
+        list_properties_or_bedrooms = []
         service = get_object_or_404(Services, pk=service)
-        for address_or_no_of_bedrooms in addresses_or_bedrooms:
+        for property_or_bedrooms in properties_or_bedrooms:
             if request.user.is_authenticated:
-                for key_address, no_of_bedrooms in address_or_no_of_bedrooms.items():
-                    the_property = get_object_or_404(Properties, street_address=key_address)
-                    total_cost = no_of_bedrooms * float(service.price)
-                    total.append(total_cost)
-                    list_addresses_or_bedrooms.append({
-                        "the_property": the_property,
-                        "total_cost": format(total_cost, '.2f'),
-                    })
-            else:
-                total_cost = address_or_no_of_bedrooms * float(service.price)
+                the_property = get_object_or_404(Properties, pk=property_or_bedrooms)
+                if service.price_variation:
+                    total_cost = the_property.no_of_bedrooms * service.price
+                else:
+                    total_cost = service.price
                 total.append(total_cost)
-                list_addresses_or_bedrooms.append({
-                    "no_of_bedrooms": address_or_no_of_bedrooms,
-                    "total_cost": format(total_cost, '.2f'),
+                list_properties_or_bedrooms.append({
+                    'property': the_property,
+                    'total_cost': total_cost,
                 })
         grand_total.append(sum(total))
         total = format(sum(total), '.2f')
         cart_items.append({
-            "service": service,
-            "properties_or_bedrooms": list_addresses_or_bedrooms,
-            "total_for_all_properties": total,
+            'service': service,
+            'properties_or_bedrooms': list_properties_or_bedrooms,
+            'total_for_all_properties': total,
         })
 
     grand_total = format(sum(grand_total), '.2f')
-
+    print(cart_items)
     context = {
             "cart_items": cart_items,
             "grand_total": grand_total,
--- cart/views.py
+++ cart/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, get_object_or_404, redirect, HttpResponse
 from services.models import Services
+from properties.models import Properties
 
 # Create your views here.
 
@@ -16,58 +17,22 @@ def add_to_cart(request, service_id):
     service = get_object_or_404(Services, pk=service_id)
     street_address = request.POST.get("street_address")
     redirect_url = request.POST.get('redirect_url')
-
-    if service.price_variation:
-        no_of_bedrooms = int(request.POST.get('no_of_bedrooms'))
-    else:
-        no_of_bedrooms = 1
+    the_property = get_object_or_404(Properties, street_address=street_address)
 
     if request.user.is_authenticated:
         if street_address:
             cart = request.session.get('cart', {})
             cart.setdefault(service_id, [])
-            street_bedrooms = {}
-            street_bedrooms[street_address] = no_of_bedrooms
-            street_bedrooms_copy = street_bedrooms.copy()
-            cart[service_id].append(street_bedrooms_copy)
+            cart[service_id].append(the_property.id)
             request.session['cart'] = cart
             print(request.session['cart'])
         else:
             print("No value.")
             return redirect(redirect_url)
-
     else:
-        cart = request.session.get('cart', {})
-        cart.setdefault(service_id, [])
-        cart[service_id].append(no_of_bedrooms)
-        request.session['cart'] = cart
-        print(request.session['cart'])
+        print("User is not logged")
+        return redirect(redirect_url)
 
     return redirect(redirect_url)
-
-
-def remove_from_cart(request, service_id):
-
-    property_address = None
-    cart = request.session.get('cart', {})
-    if 'property_address' in request.POST:
-        property_address = request.POST['property_address']
-
-    if property_address:
-        for address in cart[service_id]:
-            for address_key in address.keys():
-                if address_key == property_address:
-                    dict_address = address
-        dict_address.clear()
-        if all(not d for d in cart[service_id]):
-            cart.pop(service_id)
-    else:
-        cart.pop(service_id)
-    request.session['cart'] = cart
-
-    context = {}
-
-    return render(request, 'cart/cart.html', context)
-
 
 
