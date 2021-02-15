96d874390ca0ad7a44b8dcab961a864bcc5bdf47
Astig-1982 <astig.mandalian2014@gmail.com>
2021-02-15 10:54:25 +0000

    Aded if statement at add_property view to check whether there is already a property with the same street address

--- properties/views.py
+++ properties/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, get_object_or_404, redirect, reverse
 from .forms import PropertyForm
 from profiles.models import LandlordProfile
+from properties.models import Properties
 
 # Create your views here.
 
@@ -10,18 +11,24 @@ def add_property(request):
     landlord = get_object_or_404(LandlordProfile, user=request.user)
 
     if request.method == 'POST':
-        form_data = {
-                'landlord': landlord,
-                'street_address': request.POST['street_address'],
-                'house_name': request.POST['house_name'],
-                'post_code': request.POST['post_code'],
-                'no_of_bedrooms': request.POST['no_of_bedrooms'],
-            }
+        street_address = request.POST['street_address']
+        the_property = get_object_or_404(Properties, street_address=street_address)
+        if the_property:
+            print("there is already this property registered")
+            property_form = PropertyForm()
+        else:
+            form_data = {
+                    'landlord': landlord,
+                    'street_address': street_address,
+                    'house_name': request.POST['house_name'],
+                    'post_code': request.POST['post_code'],
+                    'no_of_bedrooms': request.POST['no_of_bedrooms'],
+                }
 
-        property_form = PropertyForm(form_data)
+            property_form = PropertyForm(form_data)
 
-        if property_form.is_valid():
-            property_form.save()
+            if property_form.is_valid():
+                property_form.save()
 
     else:
         property_form = PropertyForm()
