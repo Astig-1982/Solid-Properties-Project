030a0300482a9a8ac3b1538076d86c29513b952b
Astig-1982 <astig.mandalian2014@gmail.com>
2021-02-21 19:26:52 +0000

    Created environment variables, installed stripe and created validation errors handler

--- checkout/static/checkout/js/stripe_elements.js
+++ checkout/static/checkout/js/stripe_elements.js
@@ -2,5 +2,22 @@ let stripe_public_key = $('#id_stripe_public_key').text().slice(1, -1);
 let client_secret = $('#id_client_secret').text().slice(1, -1);
 let stripe = Stripe(stripe_public_key);
 let elements = stripe.elements();
-let cart = elements.create('card');
-cart.mount('#card-element');
+let card = elements.create('card');
+card.mount('#card-element'); 
+
+// Handle from stripe validation errors on the cart element
+
+card.addEventListener('change', function (event) {
+    var errorDiv = document.getElementById('card-errors');
+    if (event.error) {
+        var html = `
+            <span class="icon" role="alert">
+                <i class="fas fa-times"></i>
+            </span>
+            <span>${event.error.message}</span>
+        `;
+        $(errorDiv).html(html);
+    } else {
+        errorDiv.textContent = '';
+    }
+});
--- checkout/views.py
+++ checkout/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render, redirect, reverse
 from django.contrib import messages
+from django.conf import settings
 from .forms import OrderForm
+from cart.contexts import cart_contents
+
+import stripe 
 
 # Create your views here.
 
@@ -12,12 +16,16 @@ def checkout(request):
         messages.error(request, "Your shopping cart is empty.")
         return redirect(reverse('services'))
 
+    current_cart = cart_contents(request)
+    total = float(current_cart['grand_total'])
+    stripe_total = round(total)
     order_form = OrderForm()
 
     context = {
         'order_form': order_form,
         'stripe_public_key': 'pk_test_51I0QyBDK9nNxncrw48NUr7VaOFaXhP2PyitGBqK3GFVi9cxRLyg73ycKNXq3KKgOLeoSUuY8xGv5kgo1Tqjd1ThW00k5svl8xl',
         'client_secret': 'test client secret',
+        'stripe_total': stripe_total,
     }
 
     return render(request, 'checkout/checkout.html', context)
