8d47e598d01adbdc0abc7f24b011fde1e33e81e3
Astig-1982 <astig.mandalian2014@gmail.com>
2021-02-28 13:15:08 +0000

    Added country filed from django

--- profiles/views.py
+++ profiles/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, get_object_or_404
 from .models import LandlordProfile
 from properties.models import Properties
+from checkout.models import Order
 
 # Create your views here.
 
@@ -11,11 +12,13 @@ def landlord_profile(request):
     all_properties = landlord.properties.all()
     properties = all_properties.order_by('street_address')
     properties_count = all_properties.count()
+    orders = landlord.orders.all()
 
     context = {
         'landlord': landlord,
         'properties': properties,
         'properties_count': properties_count,
+        'orders': orders,
     }
 
     return render(request, 'profiles/profile.html', context)
