47475dad7c9b1c6dc5f3c9d98e974353c69bc1db
Astig-1982 <astig.mandalian2014@gmail.com>
2021-03-03 22:25:25 +0000

    Styled form fields

--- properties/forms.py
+++ properties/forms.py
@@ -8,4 +8,18 @@ class PropertyForm(forms.ModelForm):
         fields = ('landlord', 'street_address', 'house_name',
                   'post_code', 'no_of_bedrooms')
 
+    def __init__(self, *args, **kwargs):
+        """
+        Add placeholders, remove auto-generated labels
+        """
+        super().__init__(*args, **kwargs)
+        for field in self.fields:
+            self.fields[field].label = False
+            if self.fields[field].required:
+                placeholder = f'{field}*'
+            else:
+                placeholder = field
+            placeholder = placeholder.replace('_',  ' ')
+            self.fields[field].widget.attrs['placeholder'] = \
+                placeholder.capitalize()
 
