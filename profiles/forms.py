07d7688f94c283cbbf3f0274b5a7c88cc517f438
Astig-1982 <astig.mandalian2014@gmail.com>
2021-03-03 22:22:19 +0000

    Styled billing details and add_property pages

--- profiles/forms.py
+++ profiles/forms.py
@@ -9,3 +9,20 @@ class BillingForm(forms.ModelForm):
                   'default_phone_number', 'default_country',
                   'default_postcode', 'default_town_or_city',
                   'default_street_address1', 'default_street_address2',)
+
+    def __init__(self, *args, **kwargs):
+        """
+        Add placeholders, remove auto-generated labels
+        """
+        super().__init__(*args, **kwargs)
+        for field in self.fields:
+            self.fields[field].label = False
+            if self.fields[field].required:
+                placeholder = f'{field}*'
+            else:
+                placeholder = field
+            placeholder = placeholder.replace('_',  ' ')
+            placeholder = placeholder.replace('default', ' ')
+            if field != 'country':
+                self.fields[field].widget.attrs['placeholder'] = \
+                                                placeholder
