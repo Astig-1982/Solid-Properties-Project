547eb61749d6f5697f2806fdb5427172c7551b0e
Astig-1982 <astig.mandalian2014@gmail.com>
2021-03-08 20:05:10 +0000

    Added heroku secret key and set DEBUG to DEVELOPMENT if in environment

--- solid_properties/settings.py
+++ solid_properties/settings.py
@@ -23,10 +23,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'j5g@k2yj$xsqz*b6w^el*1n5!=gxdpi_7t(anb*2@q+m3)l$ho'
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
+
+# 'j5g@k2yj$xsqz*b6w^el*1n5!=gxdpi_7t(anb*2@q+m3)l$ho'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['solid-properties-project.herokuapp.com', 'localhost']
 
