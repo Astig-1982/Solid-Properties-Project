31fb7343797845f0a4da3afd5e8804855c7a5959
Astig-1982 <astig.mandalian2014@gmail.com>
2021-03-14 15:55:26 +0000

    Added count for the number of items in the cart

--- cart/contexts.py
+++ cart/contexts.py
@@ -7,8 +7,8 @@ def cart_contents(request):
 
     cart_items = []
     cart = request.session.get('cart', {})
+    count_items = []
     grand_total = 0
-
     grand_total = []
 
     for service, properties_or_bedrooms in cart.items():
@@ -24,6 +24,7 @@ def cart_contents(request):
                 else:
                     total_cost = service.price
                 total.append(total_cost)
+                count_items.append(the_property)
                 list_properties_or_bedrooms.append({
                     'property': the_property,
                     'total_cost': total_cost,
@@ -34,6 +35,7 @@ def cart_contents(request):
                 else:
                     total_cost = service.price
                 total.append(total_cost)
+                count_items.append(property_or_bedrooms)
                 list_properties_or_bedrooms.append({
                     'no_of_bedrooms': property_or_bedrooms,
                     'total_cost': total_cost,
@@ -47,11 +49,19 @@ def cart_contents(request):
             'total_for_all_properties': total,
         })
 
+    # calculate the number of items in the cart
+    if cart_items:
+        count_cart = len(count_items)
+    else:
+        count_cart = 0
+
     grand_total = format(sum(grand_total), '.2f')
     print(cart_items)
+
     context = {
             "cart_items": cart_items,
             "grand_total": grand_total,
+            "count_cart": count_cart,
         }
 
     return context
