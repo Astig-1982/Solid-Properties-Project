9e1813453113d2183602450fbcd5e57ab6518c81
Astig-1982 <astig.mandalian2014@gmail.com>
2021-03-14 11:18:42 +0000

    Deleted redundant field from 'Order' model

--- checkout/models.py
+++ checkout/models.py
@@ -25,7 +25,6 @@ class Order(models.Model):
     street_address2 = models.CharField(max_length=80, null=True, blank=True)
     date = models.DateTimeField(auto_now_add=True)
     grand_total = models.DecimalField(max_digits=10, decimal_places=2, null=False, default=0)
-    grand_total_for_anonym = models.DecimalField(max_digits=10, decimal_places=2, null=False, default=0)
 
     def _generate_order_number(self):
         """
