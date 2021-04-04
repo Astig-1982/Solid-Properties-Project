110bb756edd74b77ab8774ebabba22a2a9c97e92
Astig-1982 <astig.mandalian2014@gmail.com>
2021-04-04 13:59:29 +0000

    Added try/except block if catch exception if the value of the cart is 0

--- checkout/views.py
+++ checkout/views.py
@@ -19,93 +19,97 @@ def checkout(request):
 
     stripe_public_key = settings.STRIPE_PUBLIC_KEY
     stripe_secret_key = settings.STRIPE_SECRET_KEY
+    try:
+        if request.method == 'POST':
+            cart = request.session.get('cart', {})
+            if request.user.is_authenticated:
+                landlord = get_object_or_404(LandlordProfile, user=request.user)
+            else:
+                landlord = None
 
-    if request.method == 'POST':
-        cart = request.session.get('cart', {})
-        if request.user.is_authenticated:
-            landlord = get_object_or_404(LandlordProfile, user=request.user)
+            form_data = {
+                'landlord_profile': landlord,
+                'full_name': request.POST['full_name'],
+                'email': request.POST['email'],
+                'phone_number': request.POST['phone_number'],
+                'country': request.POST['country'],
+                'postcode': request.POST['postcode'],
+                'town_or_city': request.POST['town_or_city'],
+                'street_address1': request.POST['street_address1'],
+                'street_address2': request.POST['street_address2'],
+            }
+            order_form = OrderForm(form_data)
+            if order_form.is_valid():
+                order = order_form.save()
+                for service, properties_or_bedrooms in cart.items():
+                    service = get_object_or_404(Services, pk=service)
+                    for property_or_bedrooms in properties_or_bedrooms:
+                        if request.user.is_authenticated:
+                            the_property = get_object_or_404(Properties,
+                                                pk=property_or_bedrooms)
+                            order_line_item = OrderLineItem(
+                                order=order,
+                                service=service,
+                                the_property=the_property,
+                            )
+                            request.session['save_info'] = \
+                                'save-info' in request.POST
+                        else:
+                            order_line_item = OrderLineItemAnonym(
+                                order=order,
+                                service=service,
+                                no_of_bedrooms=property_or_bedrooms,
+                            )
+                        order_line_item.save()
+                return redirect(reverse('success_checkout',
+                                args=[order.order_number]))
+            else:
+                messages.error(request, 'There is was a problem with your form. \
+                        Please check all fields and try again.')
         else:
-            landlord = None
+            cart = request.session.get('cart', {})
+            if not cart:
+                messages.error(request, "Your shopping cart is empty.")
+                return redirect(reverse('services'))
 
-        form_data = {
-            'landlord_profile': landlord,
-            'full_name': request.POST['full_name'],
-            'email': request.POST['email'],
-            'phone_number': request.POST['phone_number'],
-            'country': request.POST['country'],
-            'postcode': request.POST['postcode'],
-            'town_or_city': request.POST['town_or_city'],
-            'street_address1': request.POST['street_address1'],
-            'street_address2': request.POST['street_address2'],
-        }
-        order_form = OrderForm(form_data)
-        if order_form.is_valid():
-            order = order_form.save()
-            for service, properties_or_bedrooms in cart.items():
-                service = get_object_or_404(Services, pk=service)
-                for property_or_bedrooms in properties_or_bedrooms:
-                    if request.user.is_authenticated:
-                        the_property = get_object_or_404(Properties,
-                                            pk=property_or_bedrooms)
-                        order_line_item = OrderLineItem(
-                            order=order,
-                            service=service,
-                            the_property=the_property,
-                        )
-                        request.session['save_info'] = \
-                            'save-info' in request.POST
-                    else:
-                        order_line_item = OrderLineItemAnonym(
-                            order=order,
-                            service=service,
-                            no_of_bedrooms=property_or_bedrooms,
-                        )
-                    order_line_item.save()
-            return redirect(reverse('success_checkout',
-                            args=[order.order_number]))
-        else:
-            messages.error(request, 'There is was a problem with your form. \
-                    Please check all fields and try again.')
-    else:
-        cart = request.session.get('cart', {})
-        if not cart:
-            messages.error(request, "Your shopping cart is empty.")
-            return redirect(reverse('services'))
+            current_cart = cart_contents(request)
+            total = float(current_cart['grand_total'])
+            stripe_total = round(total * 100)
+            stripe.api_key = stripe_secret_key
+            intent = stripe.PaymentIntent.create(
+                amount=stripe_total,
+                currency=settings.STRIPE_CURRENCY,
+            )
 
-        current_cart = cart_contents(request)
-        total = float(current_cart['grand_total'])
-        stripe_total = round(total * 100)
-        stripe.api_key = stripe_secret_key
-        intent = stripe.PaymentIntent.create(
-            amount=stripe_total,
-            currency=settings.STRIPE_CURRENCY,
-        )
+            if request.user.is_authenticated:
+                landlord = get_object_or_404(LandlordProfile, user=request.user)
+                order_form = OrderForm(initial={
+                        'full_name': landlord.default_full_name,
+                        'email': landlord.user.email,
+                        'phone_number': landlord.default_phone_number,
+                        'country': landlord.default_country,
+                        'postcode': landlord.default_postcode,
+                        'town_or_city': landlord.default_town_or_city,
+                        'street_address1': landlord.default_street_address1,
+                        'street_address2': landlord.default_street_address2,
+                    })
+            else:
+                order_form = OrderForm()
 
-        if request.user.is_authenticated:
-            landlord = get_object_or_404(LandlordProfile, user=request.user)
-            order_form = OrderForm(initial={
-                    'full_name': landlord.default_full_name,
-                    'email': landlord.user.email,
-                    'phone_number': landlord.default_phone_number,
-                    'country': landlord.default_country,
-                    'postcode': landlord.default_postcode,
-                    'town_or_city': landlord.default_town_or_city,
-                    'street_address1': landlord.default_street_address1,
-                    'street_address2': landlord.default_street_address2,
-                })
-        else:
-            order_form = OrderForm()
+            if not stripe_public_key:
+                messages.warning(request, 'Stripe public key is missing.')
 
-        if not stripe_public_key:
-            messages.warning(request, 'Stripe public key is missing.')
+            context = {
+                'order_form': order_form,
+                'stripe_public_key': stripe_public_key,
+                'client_secret': intent.client_secret,
+            }
 
-        context = {
-            'order_form': order_form,
-            'stripe_public_key': stripe_public_key,
-            'client_secret': intent.client_secret,
-        }
+        return render(request, 'checkout/checkout.html', context)
 
-    return render(request, 'checkout/checkout.html', context)
+    except Exception:
+        messages.warning(request, "purchases cannot be performed on free services only.")
+        return redirect(reverse('cart'))
 
 
 def success_checkout(request, order_number):
